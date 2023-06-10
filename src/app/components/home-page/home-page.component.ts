1a387e836c206b155890f2d5a1f92905622cf51e
Anastasiia <troyanovskaya2003@gmail.com>
2023-06-10 22:39:06 +0100

    additional banners

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -12,7 +12,12 @@ export class HomePageComponent implements OnInit {
   //   {url: 'https://www.stationerybazaar.com/images/pzen_slideshow/colorful-stationery-multicolored-background_1623598949.jpg', title: 'img3'},
   //   {url: 'https://i2.wp.com/www.paperchainweddingstationery.co.uk/wp-content/uploads/2020/08/Greenery-Slider-1200-600-2.png?fit=1200%2C600&ssl=1', title: 'img4'}
   // ];
-  slides = [{url: 'assets/banner_1.jpg', title: 'banner_1'}]
+  slides = [{url: 'assets/banner_1.jpg', title: 'banner_1'},
+  {url: 'assets/banner_2.jpg', title: 'banner_2'},
+  {url: 'assets/banner_3.jpg', title: 'banner_3'},
+  {url: 'assets/banner_4.jpg', title: 'banner_4'},
+  {url: 'assets/banner_5.jpg', title: 'banner_5'}
+]
   ngOnInit(){
     window.scrollTo(0, 0)
   }
